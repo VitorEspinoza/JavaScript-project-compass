f56b9785718cfed7f80d542b3366ae73ddb88ec5
Espinoza <vitorespinoza18@gmail.com>
2022-06-06 00:09:12 -0300

    Validações funcionando

--- js/app/lib/addCertificates.js
+++ js/app/lib/addCertificates.js
@@ -13,21 +13,33 @@ btnAddCertificate.addEventListener('click', (event) => {
         let newInputCertificate = document.createElement('input');
         let newHeartIcon = document.createElement('img');
         let newContainerCertificate = document.createElement('div');
+        let newParentContainer = document.createElement('div');
+        let newTrashIcon = document.createElement('img');
 
         newInputCertificate.classList.add('certificate-input');
+        newInputCertificate.classList.add('not-favorite');
         newContainerCertificate.classList.add('certificateContainer');
-        
+        newParentContainer.classList.add('flex');
+        newParentContainer.classList.add('container-certificados');
+
         newHeartIcon.src = "./img/heart.svg";
         newHeartIcon.classList.add('heart-icon');
+        newTrashIcon.src = "./assets/trash.svg";
+        newTrashIcon.classList.add('trash-icon')
          
         newInputCertificate.value = constructorCertificate.value;
         constructorCertificate.value = "";
         newInputCertificate.readOnly = true;
-    
+
+        
         newContainerCertificate.appendChild(newInputCertificate);
         newContainerCertificate.appendChild(newHeartIcon);
 
-        certificates.appendChild(newContainerCertificate);
+        newParentContainer.appendChild(newContainerCertificate);
+        newParentContainer.appendChild(newTrashIcon);
+        
+        
+        certificates.appendChild(newParentContainer);
     } 
     else if (certificates.querySelectorAll('.quantity-error').length == 0 && btnAddCertificate.Disabled == true) {    
         let errorQuantCertificates = document.createElement('span');
@@ -40,17 +52,22 @@ btnAddCertificate.addEventListener('click', (event) => {
     
 });
 
-certificates.addEventListener('mousedown', () => {
+certificates.addEventListener('click', () => {
     let favorites = document.querySelectorAll('.heart-icon');
 
     favorites.forEach(element => {
+        
         element.addEventListener('click', () => {
             element.classList.toggle('favorite-true');
             if (element.classList.contains('favorite-true')) {
                 element.src = "img/heart-favorite.svg";
+                element.parentNode.querySelector("input").classList.add("favorite");
+                element.parentNode.querySelector("input").classList.remove("not-favorite");
             }
             else {
                 element.src = "img/heart.svg";
+                element.parentNode.querySelector("input").classList.add("not-favorite");
+                element.parentNode.querySelector("input").classList.remove("favorite");
             }
         });
     });
--- js/app/lib/validation.js
+++ js/app/lib/validation.js
@@ -1,71 +1,66 @@
-console.log(saveInfosController.SaveQualifications);
-
-function ValidationButton(listElements, regex, form) {
-  let btn = form.querySelector('.btn-post');
-  listElements.forEach(element => {
-    if(element.classList.contains('required')){
-      if (regex.test(element.value)) {
-        btn.classList.add('btnActive');
-        btn.disabled = false;
-      }
-      else {
-        btn.classList.remove('btnActive');
-        btn.disabled = true;
-      }
-    }
-    
-  });
+function ValidationField(regex, field, form) {
+ 
+  if (regex.test(field.value) || field.value == "")
+  {
+    field.parentNode.querySelector('span').classList.add('invisible');
+  
+  }
+    else {
+    field.parentNode.querySelector('span').classList.remove('invisible');
+     if (field.classList.contains("required")) {  
+      let btnSubmit = form.querySelector('.btn-post');   
+       btnSubmit.classList.remove('btnActive');
+       btnSubmit.disabled = true; 
+     }
+  }
 }
 
+function ValidationForm(form, e){
+  regUrl = new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
+  regName = new RegExp(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/);
+  regEmail = new RegExp( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+ 
+  var listRequiredInputs = form.querySelectorAll(".required");
 
-function ValidationField(regex, field) {
-  if (regex.test(field.value)) {
-    field.parentNode.querySelector('span').classList.add('invisible');
+  
+  let isValidBtn = true;
+  
+  listRequiredInputs.forEach(input => {
+ 
+  if (!input.parentNode.querySelector('span').classList.contains("invisible") || input.value == "" || (input.classList.contains("input-check") && !input.checked)) {  
+    isValidBtn = false;
+  } 
+
+  });
+  let btnSubmit = form.querySelector('.btn-post');
+  if (isValidBtn) {
+    btnSubmit.classList.add('btnActive');
+    btnSubmit.disabled = false;
   }
   else {
-    field.parentNode.querySelector('span').classList.remove('invisible');
+    btnSubmit.classList.remove('btnActive');
+    btnSubmit.disabled = true; 
+  }
+
+  if (e.target.classList.contains('input-url')) {
+    ValidationField(regUrl, e.target, form);   
+  }
+  if (e.target.classList.contains('input-name')) {
+    ValidationField(regName, e.target, form);
+  }
+  if (e.target.classList.contains('input-email')) {
+    ValidationField(regEmail, e.target, form);
   }
 }
-
 const forms = document.querySelectorAll('form');
 
 forms.forEach(form => {
   form.querySelectorAll('input').forEach(input => {
     input.addEventListener('input', (e) => {
-
-      regUrl = new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
-      regName = new RegExp(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/);
-      regEmail = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-     
-      var listUrls = form.querySelectorAll(".input-url");
-      var listNames = form.querySelectorAll(".input-name");
-      var listEmails = form.querySelectorAll(".input-email");
-    
-      
-      if (listUrls.length > 0) {
-        ValidationButton(listUrls, regUrl, form);
-      }
-      if (listNames.length > 0) {
-        ValidationButton(listNames, regName, form);
-      }
-    
-      if (listEmails.length > 0) {
-        ValidationButton(listEmails, regEmail, form);
-      }
-    
-      if (e.target.classList.contains('input-url')) {
-        ValidationField(regUrl, e.target);   
-      }
-      if (e.target.classList.contains('input-name')) {
-        ValidationField(regName, e.target);
-      }
-      if (e.target.classList.contains('input-email')) {
-        ValidationField(regEmail, e.target);
-      }
-
-    })
+      ValidationForm(form, e);
+    }) 
+    input.addEventListener('keyup', (e) => {
+      ValidationForm(form, e);
+    }) 
   })
 });
-
-
-
